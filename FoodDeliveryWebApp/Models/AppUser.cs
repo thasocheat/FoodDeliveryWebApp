667de6c6b47691871dfbfc9a76ba272aba72e4f8
thasocheat <thasocheat167@gmail.com>
2024-01-16 14:23:02 +0700

    update on model

--- FoodDeliveryWebApp/Models/AppUser.cs
+++ FoodDeliveryWebApp/Models/AppUser.cs
@@ -2,5 +2,15 @@
 {
     public class AppUser
     {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public string Password { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreateAt { get; set; }
     }
 }
